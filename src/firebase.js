12f39da3a39106930ed0bf998161dfd7bf3d0ea8
unknown <ash.990.007@gmail.com>
2021-12-31 19:13:08 +0530

    Removed Personal API Keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBrLTYv2XPeBJwz_Trb4ufo5y7F8xdXKqo",
-    authDomain: "app-76b89.firebaseapp.com",
-    projectId: "app-76b89",
-    storageBucket: "app-76b89.appspot.com",
-    messagingSenderId: "207158784032",
-    appId: "1:207158784032:web:bc4be7dad645e2c1ed4c45"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
